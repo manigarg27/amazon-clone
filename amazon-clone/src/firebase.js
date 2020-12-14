6de69942ac3e0f3207d507465d8df5b7c730a2c9
manigarg27 <47330599+manigarg27@users.noreply.github.com>
2020-12-14 21:26:57 +0530

    header frontend

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCBYR-Iav5xXDEUii8KoRfIzRxGJKFb-nM",
+  authDomain: "clone-b6282.firebaseapp.com",
+  projectId: "clone-b6282",
+  storageBucket: "clone-b6282.appspot.com",
+  messagingSenderId: "179416618597",
+  appId: "1:179416618597:web:3512e4fffe42f55e04588c",
+  measurementId: "G-3MXNXSGMR5"
+};
